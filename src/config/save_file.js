fe0d7a0b17e7a64bb000cb5059eb01c9a9899986
quhongbin <2818777520@qq.com>
2025-10-27 19:30:58 +0800

    '添加中间件multer处理文件上传，增添配置文件save_file.js统一配置文件上传相关内容'

---
+++ src/config/save_file.js
@@ -0,0 +1,7 @@
+export const WINDOWS ={
+    dir:'uploads\\'
+}
+
+export const LINUX ={
+    dir:'/tmp/my-uploads/'
+}
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,11 +1,37 @@
 import { Router as _Router } from 'express'
+import multer from 'multer'
+import os from 'os'
+import fs from 'fs'
+import {WINDOWS,LINUX} from '../config/save_file.js'
+
+import {getAllPosts,deletePost,getPostsCounts,createPost}  from '../controllers/postController.js'
+
 const router = _Router()
-import {getAllPosts,deletePost,getPostsCounts}  from '../controllers/postController.js'
-
-
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (os.type()=== 'Windows_NT') {
+        if (!fs.existsSync('uploads/')){
+            fs.mkdirSync('uploads/');
+        }
+        cb(null, 'uploads/')
+    }else if (os.type() === 'Linux') {
+        if (!fs.existsSync(LINUX.dir)){
+            fs.mkdirSync(LINUX.dir);
+        }
+        cb(null, LINUX.dir)
+    }
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + '_' + new Date().toISOString()
+    .replace(/\..*/g, '')
+    .replace('T', '_')
+    .replace(':', '-'))
+  }
+})
+const upload = multer({ storage: storage })
 
 router.get('/all',getAllPosts)
 router.delete('/:id',deletePost)
 router.get('/count',getPostsCounts)
-
+router.post('/create',upload.single('file'),createPost)
 export default router
